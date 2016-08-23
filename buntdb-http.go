f729adcb72fec49a7bc4487ede9d3a27da8a3d25
Brad Serbu <bradserbu@bradserbu.com>
2016-08-22 23:44:00 -0400

    Added basic content-type support for JSON values.

--- buntdb-http.go
+++ buntdb-http.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"log"
@@ -15,7 +16,19 @@ func main() {
 	router.HandleFunc("/{database}/{key}", get).Methods("GET")
 	router.HandleFunc("/{database}/{key}", set).Methods("POST")
 	router.HandleFunc("/{database}", create).Methods("PUT")
+
 	log.Fatal(http.ListenAndServe(":8080", router))
+}
+
+func isJSONString(s string) bool {
+	var js string
+	return json.Unmarshal([]byte(s), &js) == nil
+
+}
+
+func isJSON(s string) bool {
+	var js map[string]interface{}
+	return json.Unmarshal([]byte(s), &js) == nil
 }
 
 func open(database string) (db *buntdb.DB, err error) {
@@ -89,8 +102,27 @@ func get(w http.ResponseWriter, r *http.Request) {
 			return err
 		}
 
-		w.Header().Set("Content-Type", "application/json")
-		w.WriteHeader(http.StatusOK)
+		contentType := r.Header.Get("Content-Type")
+		log.Println(fmt.Sprintf("content-type:%s", contentType))
+
+		// application/json
+		if contentType == "application/json" {
+			w.Header().Set("Content-Type", "application/json")
+			w.WriteHeader(http.StatusOK)
+			fmt.Fprintln(w, value)
+			return nil
+		}
+
+		// auto-detect json
+		if contentType == "" {
+			if isJSONString(value) || isJSON(value) {
+				w.Header().Set("Content-Type", "application/json")
+				w.WriteHeader(http.StatusOK)
+				fmt.Fprintln(w, value)
+				return nil
+			}
+		}
+
 		fmt.Fprintln(w, value)
 		return nil
 	})
